4041a94eb5a591e01834a14debe3c3e3ade4bd5c
Henning Bredel <h.bredel@52north.org>
2022-11-10 16:07:38 +0000

    Adds external application to "add resource"

--- externalapplications/models.py
+++ externalapplications/models.py
@@ -8,3 +8,7 @@ from geonode.geoapps.models import GeoApp
 # Create your models here.
 class ExternalApplication(GeoApp):
     url = models.URLField(max_length=2000, null=False, blank=False, help_text="Link to the external application")
+
+    def get_absolute_url(self):
+        return f"/external-applications/{self.pk}"
+        
