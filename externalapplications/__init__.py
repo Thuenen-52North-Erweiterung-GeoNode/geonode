a0be319183ce65c2d0775618a809ec8c7b942d85
Henning Bredel <h.bredel@52north.org>
2022-11-10 13:02:16 +0000

    Declare externalapplication as geonode app

--- externalapplications/__init__.py
+++ externalapplications/__init__.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 class ExternalapplicationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'externalapplications'
+    type = 'GEONODE_APP'
+    default_model = 'ExternalApplication'
+
+default_app_config = 'externalapplications.ExternalapplicationsConfig'
