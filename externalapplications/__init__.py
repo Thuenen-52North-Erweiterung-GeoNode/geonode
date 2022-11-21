43976ad3f199bfa7222f63a4ea7dd5e3c2310ee8
Christian Autermann <c.autermann@52north.org>
2022-11-21 14:35:59 +0100

    dynamically calculate menu order

--- externalapplications/__init__.py
+++ externalapplications/__init__.py
@@ -1,7 +1,10 @@
 import os
 from django.apps import AppConfig
 from django.conf.urls import include, url
+from django.db.models import Max
 
+import logging
+logger = logging.getLogger(__name__)
 
 def run_setup_hooks(*args, **kwargs):
     from django.conf import settings
@@ -11,11 +14,18 @@ def run_setup_hooks(*args, **kwargs):
     LOCAL_ROOT = os.path.abspath(os.path.dirname(__file__))
     settings.MAPSTORE_TRANSLATIONS_PATH += ("/static/mapstore/ea-translations",)
     settings.TEMPLATES[0]["DIRS"].insert(0, os.path.join(LOCAL_ROOT, "templates"))
+    title = "External Applications"
 
-    if not Menu.objects.filter(title="External Applications").exists():
+    if not Menu.objects.filter(title=title).exists():
         ph = MenuPlaceholder.objects.filter(name="TOPBAR_MENU_LEFT").first()
-        menu = Menu.objects.create(title="External Applications", placeholder=ph, order=1)
-        MenuItem.objects.create(title="External Applications", menu=menu, order=1, blank_target=False,
+        if (not ph):
+            logger.info(f"MenuPlaceholder not yet created. Skipping")
+            return
+
+        max_order = Menu.objects.filter(placeholder=ph).aggregate(Max("order"))["order__max"]
+        order = 0 if max_order is None else max_order + 1
+        menu = Menu.objects.create(title=title, placeholder=ph, order=order)
+        MenuItem.objects.create(title=title, menu=menu, order=1, blank_target=False,
                                 url="/catalogue/#/search/?f=externalapplication")
 
     urlpatterns += [url(r'^external-applications/', include('externalapplications.urls'))]
